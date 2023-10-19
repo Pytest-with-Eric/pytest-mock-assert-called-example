0c1b54264ff4950facffe4b492f47523f1cf6e74
ericsalesdeandrade <sdaeric19@gmail.com>
2023-10-19 12:07:54 +0800

    Formatting

--- tests/test_pytest_mock_assert_patch.py
+++ tests/test_pytest_mock_assert_patch.py
@@ -1,15 +1,18 @@
 from unittest.mock import patch
 
+
 class Calculator:
     def add(self, a, b):
         return a + b
+
 
 def get_total_price(price, quantity):
     calc = Calculator()
     return calc.add(price, quantity)
 
+
 # Patching a function as a decorator
-@patch.object(Calculator, 'add')
+@patch.object(Calculator, "add")
 def test_with_decorator(mock_add):
     # The 'mock_add' argument is automatically passed with the patched object
 
@@ -22,9 +25,10 @@ def test_with_decorator(mock_add):
     # Verify the result using the patched 'add' method
     assert result == 10
 
+
 # Patching an object's attribute as a context manager
 def test_with_context_manager():
-    with patch.object(Calculator, 'add') as mock_add:
+    with patch.object(Calculator, "add") as mock_add:
         # Set the return value for the mocked 'add' method
         mock_add.return_value = 15
 
@@ -34,8 +38,9 @@ def test_with_context_manager():
     # Verify the result using the patched 'add' method
     assert result == 15
 
+
 # Patching a class method as a decorator
-@patch.object(Calculator, 'add', autospec=True)
+@patch.object(Calculator, "add", autospec=True)
 def test_class_method(mock_add):
     # The 'mock_add' argument is automatically passed with the patched object
 
@@ -47,8 +52,3 @@ def test_class_method(mock_add):
 
     # Verify the result using the patched 'add' method
     assert result == 20
-
-if __name__ == '__main__':
-    test_with_decorator()  # Test with decorator
-    test_with_context_manager()  # Test with context manager
-    test_class_method()  # Test class method
